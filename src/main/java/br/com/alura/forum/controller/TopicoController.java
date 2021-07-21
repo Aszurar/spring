b00c5358939dfe804531a976f89856a3e05eb48f
Lucas de Lima <lms.souza39@gmail.com>
2021-07-21 15:20:33 -0300

    :tada: repository :tada:

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -1,22 +1,23 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicoController {
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     public List<TopicoDto> list() {
-        Topico topico = new Topico("Duvida","Duvida com Spring", new Curso("Spring", "Programação"));
-        Topico topico1 = new Topico("Duvida","Duvida com React", new Curso("React", "Programação"));
-        Topico topico2 = new Topico("Duvida","Duvida com React-Native", new Curso("React-Native", "Programação"));
-        
-        return TopicoDto.converter(Arrays.asList(topico, topico1, topico2));
+        List<Topico> topicos = topicoRepository.findAll();
+
+        return TopicoDto.converter(topicos);
     }
 }
